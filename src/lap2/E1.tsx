d64a3412a9decf7b6e57dd22cc4f4a426f9bed1a
hieube-deep <nguyentrunghieu11032006@gmail.com>
2026-03-12 15:18:19 +0700

    lap2

--- src/lap2/E1.tsx
+++ src/lap2/E1.tsx
@@ -1,22 +1,41 @@
 import React from 'react'
-import { Table } from 'antd'
+import { Table, Button } from 'antd'
 function E1() {
     const columns = [
         { title: "ID", dataIndex: "ID" },
         { title: "Name", dataIndex: "Name" },
-        { title: "Age", dataIndex: "Age" },
-        { title: "Major", dataIndex: "Major" },
+        { title: "Email", dataIndex: "Email" },
+        {
+            title: "Action",
+            render: (_: any, record: any) => (
+                <>
+                    <Button type='primary'>Edit</Button>
+                    <Button type='primary'>xóa</Button>
+                </>
+            ),
+        },
+        {
+            title: "Status", dataIndex: "Status",
+            render: (Status: any) => {
+                return (<span style={{ color: Status === "active" ? "green" : "red" }}>
+                    {Status}
+                </span >)
+            }
+        },
     ];
 
     const data = [
-        { key: 1, ID: "1", Name: "A", Age: 12, Major: "Cnnt" },
-        { key: 2, ID: "2", Name: "B", Age: 20, Major: "Cnnt" },
-        { key: 3, ID: "3", Name: "C", Age: 16, Major: "Cnnt" },
-        { key: 4, ID: "4", Name: "D", Age: 12, Major: "Cnnt" },
+        { key: 1, ID: "1", Name: "A", Email: "a@gmail.com" },
+        { key: 2, ID: "2", Name: "B", Email: "b@gmail.com" },
+        { key: 3, ID: "3", Name: "C", Email: "c@gmail.com" },
+        { key: 4, ID: "4", Name: "D", Email: "d@gmail.com" },
+        { key: 5, ID: "5", Name: "E", Email: "e@gmail.com" },
+
+
     ]
     return (
         <div>
-            <Table columns={columns} dataSource={data} />
+            <Table columns={columns} dataSource={data} pagination={{ pageSize: 3 }} />
         </div>
     )
 }
